8a2603b32ab95768879ee6f6d860bb39a219cd39
Jorrit Boekel <jorrit.boekel@scilifelab.se>
2013-03-07 10:56:13 +0100

    Changed to running as loop

--- sync_transfer.py
+++ sync_transfer.py
@@ -1,14 +1,16 @@
 """
 This script will sync 2 folders if the destination folder is a mount point.
 The objective is to avoid syncing to a mounted share when it is not mounted.
-Preferably to be run as a cron job.
+Will run as an infinite loop, to avoid multiple spawned processes that may
+occur if the sync is slower than the crontab time.
+Therefore detached running or start from init script is advisable.
 
 Usage:
     python sync_to_mount sourcefolder destinationfolder
 
 """
 
-import os, sys, logging
+import os, sys, logging, time
 
 log = logging.getLogger('sync_to_mount')
 log.setLevel(10)
@@ -25,10 +27,13 @@ if len(sys.argv) != 3:
 src = sys.argv[1]
 dst = sys.argv[2]
 
-# check mounts
-if not os.path.ismount(dst):
-    log.warning('Destination folder is not a mount point. Will not sync now.')
-    sys.exit(1)
-else:
-    os.system('rsync -lts -r /mnt/datadrive/ /mnt/kalevalatmp/')
+while True:
+    # check mounts
+    if not os.path.ismount(dst):
+        log.warning('Destination folder is not a mount point. Will not sync now.')
+    else:
+        log.info('Syncing {0} folder to destination {1}'.format(src, dst))
+        os.system('rsync -lts -r /mnt/datadrive/ /mnt/kalevalatmp/')
+    
+    time.sleep(900)
 
